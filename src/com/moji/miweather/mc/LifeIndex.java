a99bf68ed52db23d2672ba22593b51c803da0d0b
cong.wang <cong.wang@moji.com>
2016-11-18 12:06:13 +0800

    version 1.1

---
+++ src/com/moji/miweather/mc/LifeIndex.java
@@ -0,0 +1,10 @@
+package com.moji.miweather.mc;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LifeIndex extends BasePage {
+	
+}
---
+++ src/com/moji/miweather/mc/MainPage.java
@@ -0,0 +1,34 @@
+package com.moji.miweather.mc;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class MainPage extends BasePage {
+
+	@Test
+	public void test() throws InterruptedException, IOException {
+		startRecord();
+		new MainPage().newTutorial();
+		
+		CityManagerPage cmp = new CityManagerPage();
+		cmp.addCity();
+		// System.out.println("天气首页："+driver.getTitle());
+		/*PersonPage loginObj = new PersonPage();
+		loginObj.loginPhone(driver);*/
+		/*
+		 * BasePage i = new homepage(); BasePage j = new LivePage();
+		 * ArrayList<BasePage> aa = new ArrayList<>(); aa.add(i); aa.add(j);
+		 * for(int k = 0;k< aa.size() ;k++){ BasePage as = aa.get(k);
+		 * as.homePage(driver); }
+		 */
+		/*
+		 * BaseMethods i= new BaseMethods(); System.out.println("开始录屏");
+		 * i.startRecord(); System.out.println("结束录屏"); i.share(driver);
+		 * i.snapshot((TakesScreenshot)driver, "分享页面");
+		 */
+
+	}
+}
